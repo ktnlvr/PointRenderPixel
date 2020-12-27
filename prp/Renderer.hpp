3e4d9762a865a326111778d6730e7861dc309e19
youngman114 <artur.roos@mail.ru>
2020-12-27 13:06:44 +0300

    Singletoned Renderer

--- prp/Renderer.hpp
+++ prp/Renderer.hpp
@@ -1,10 +1,26 @@
 #pragma once
 
+#include <memory>
+
 namespace prp {
-	class Renderer {
+	class Renderer : std::enable_shared_from_this<Renderer> {
+#pragma region SINGLETON
+	public:
+		static std::shared_ptr<Renderer> GetInstance() noexcept {
+			if (instance.get() == nullptr) return std::shared_ptr<Renderer>(new Renderer());
+			else return instance;
+		}
+
+	protected:
+		inline static std::shared_ptr<Renderer> instance;
+
+		Renderer() {}
+
+#pragma endregion
+
 #pragma region RULE OF FIVE
 	public:
-		~Renderer() = delete;
+		~Renderer() = default;
 		
 		Renderer(Renderer&&) = delete;
 		Renderer(const Renderer&) = delete;
@@ -14,4 +30,8 @@ namespace prp {
 
 #pragma endregion
 	};
+
+	typedef std::shared_ptr<Renderer> SharedRenderer;
+
+	inline auto GetRendererInstance() noexcept -> SharedRenderer { return Renderer::GetInstance(); }
 }
